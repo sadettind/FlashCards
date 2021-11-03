575debe851f3547d475d620d413cc2cd07000ff3
sadettind <demirtas@usc.edu>
2021-11-03 15:07:45 +0100

    Add files via upload

---
+++ stage 7/Main.java
@@ -0,0 +1,13 @@
+package flashcards;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        FlashCards flashCard = new FlashCards(args);
+
+        flashCard.start();
+    }
+
+}
+
